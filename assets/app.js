0802f9851194578eabe55d86dae3b7c5dd32b9a7
Ousseine <ousseinetech@gmail.com>
2021-12-05 13:39:07 +0100

    add files into public and assets

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+// any CSS you import will output into a single css file (app.css in this case)
+import './css/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+// start boostrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min';
+
+// fontawesome
+import '@fortawesome/fontawesome-free/css/all.min.css'
